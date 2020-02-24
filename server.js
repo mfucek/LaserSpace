520a4d3aaed06446c962d1bed3013388724cb02d
matijafucek1 <matijafucek1@gmail.com>
2020-02-24 22:16:20 +0100

    potential node fix

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-var express = require('express'),
-app = express(); 
-app.use('/', express.static(__dirname + '/'));
-app.listen(8080);
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.sendFile( __dirname + '/index.html' )
+})
+
+// app.get('/Assets/CSS/style.css', (req, res) => {
+//   res.sendFile( __dirname + '/Assets/CSS/style.css' )
+// })
+
+// app.get('/Assets/JS/', (req, res) => {
+//   res.sendFile( __dirname + '/Assets/JS/' )
+// })
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log("Listening on " + PORT);  
+})
+
+
