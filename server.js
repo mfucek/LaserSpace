ee6840b2d2bb19a7ac7609fb52eb32af00904dec
matijafucek1 <matijafucek1@gmail.com>
2020-02-24 21:42:45 +0100

    nodejs now pulls game

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Listening on " + PORT);  
-})
-
-app.get('/', (req, res) => {
-  res.send({ ok: true })
-})
+var express = require('express'),
+app = express(); 
+app.use('/', express.static(__dirname + '/'));
+app.listen(8080);
